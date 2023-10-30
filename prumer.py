aa919937e342307493fb40305e0bab7ffebe5c73
Adam Štrajt <strajtoa@arcig.cz>
2023-10-30 14:54:20 +0100

    vscode commitovani

--- prumer.py
+++ prumer.py
@@ -1,9 +1,19 @@
 import sys
 #hahaha zmena z poicitace na github push
+#haha zmena ve vscode
+"""
 def prumer(cislo):
     soucet=0
     for i in cislo:
         soucet+=i
     return(soucet/len(cislo))
 
-print(prumer(sys.argv[1:]))
+print(prumer(sys.argv[1:]))"""
+
+def suma(cislo):
+    soucet=0
+    for i in cislo:
+        soucet+=i
+    return soucet
+
+print(suma(sys.argv[1:]))
